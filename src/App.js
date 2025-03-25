a10ceef68305e409a14e5cea4257461b57374d42
djhanus <davidjhanus@gmail.com>
2025-03-25 18:29:14 -0400

    add new element

--- src/App.js
+++ src/App.js
@@ -9,6 +9,9 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        <p>
+          Welcome to project lift off!!
+        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +20,22 @@ function App() {
         >
           Learn React
         </a>
+        <h1>Project Lift Off</h1>
+        <h2>Subheading Example</h2>
+        <p>This is a paragraph with some additional text to demonstrate basic markup.</p>
+        <ul>
+          <li>First item</li>
+          <li>Second item</li>
+          <li>Third item</li>
+        </ul>
+        <ol>
+          <li>Ordered item one</li>
+          <li>Ordered item two</li>
+          <li>Ordered item three</li>
+        </ol>
+        <blockquote>
+          "This is a blockquote example to showcase styled text."
+        </blockquote>
       </header>
     </div>
   );
