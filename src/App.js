8e037305d75894abcd018bb67efcdd367d169532
djhanus <davidjhanus@gmail.com>
2025-03-25 21:42:48 -0400

    props

--- src/App.js
+++ src/App.js
@@ -2,26 +2,27 @@
 import './App.css';
 
 
-function Header() {
+function Header(props) {
+  console.log(props)
   return (
     <header>
-      <h1>Banjo Kazooie</h1>
+      <h1>{props.name} Kazooie</h1>
     </header>
   );
 }
 
-function Main() {
+function Main(props) {
   return (
     <section>
-      <p>Gruntilda's Lair, Spiral Mountain, Click Clock Wood, Bubblegloop Swamp, Treasure Trove Cove.</p>
+      <p>Gruntilda's Dungeon is {props.adjective}, a haven for weary adventurers. Nestled between the Whispering Woods and the Mystic Marsh, it offers tales of heroism, hearty meals, and the finest mead in the realm.</p>
     </section>
     );
 }
 
-function Footerios() {
+function Footerios(props) {
   return (
     <section>
-      <p>@ This is the footer</p>
+      <p>@ {props.yearrr}</p>
     </section>
     );
 }
@@ -29,9 +30,9 @@ function Footerios() {
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footerios />
+      <Header name="Proterius" />
+      <Main adjective="delicious" />
+      <Footerios yearrr={new Date().getFullYear()}/>
     </div>
   );
 }
