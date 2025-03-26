8a2300a97b3cf87aed4e8dd881d5373b55eef39e
djhanus <davidjhanus@gmail.com>
2025-03-26 15:28:55 -0400

    useeffect usestate

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {  
   const [emotion, setEmotion] = useState("happy");
-  console.log(emotion)
+  const [secondary, setSecondary] = useState("tired");
+
+  useEffect(() => {
+    console.log(`It's ${emotion} around here!`);
+  }, [emotion]);
+
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
+
   return (
     <>
-      <h1>Current state of emotions: {emotion}.</h1>
+      <h1>Current state of emotions: {emotion} and {secondary}.</h1>
       <button onClick={() => setEmotion("frustrated")}>
         Frustrate!
       </button>
-      <button onClick={() => setEmotion("exhuberant")}>
-        Exhuberate!!
+      <button onClick={() => setSecondary("ridiculous")}>
+        Ridiculate!
+      </button>
+      <button onClick={() => setEmotion("slappy")}>
+        Slappillate!
       </button>
     </>
   );
--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
-const [,, light] = ["boots", "tent", "headlamp", "map"];
-console.log(light)
+// const [,, light] = ["boots", "tent", "headlamp", "map"];
+// console.log(light)
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
