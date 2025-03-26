510bae05b84e6ce0576c3ef9da3cf52b39ba0e27
djhanus <davidjhanus@gmail.com>
2025-03-25 23:12:00 -0400

    fragment apptwo

--- src/index.js
+++ src/index.js
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 
+function AppTwo() {
+  return <h1>Second App Header</h1>
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <App />
+  <>
+    <App />
+    <AppTwo />
+  </>
 );
-
