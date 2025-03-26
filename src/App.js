afdfe8849d0ec14c8578601375f6293dd22766f5
djhanus <davidjhanus@gmail.com>
2025-03-26 15:17:03 -0400

    const setemotion

--- src/App.js
+++ src/App.js
@@ -1,27 +1,20 @@
+import React, { useState } from 'react';
 import './App.css';
 
-
-function SecretComponent() {
-  return <h1>Super secret information for authorized users only.</h1>;
-}
-
-function RegularComponent() {
-  return <h1>Everyone can see this component.</h1>;
-}
-
-function App(props) {  
-  if(props.authorized) {
-    return <SecretComponent />
-  } else {
-    return <RegularComponent />;
-  }
-
-  // return (
-  //   <>
-  //   { props.authorized ? <SecretComponent /> : <RegularComponent /> }
-  //   </>
-  // );
-
+function App() {  
+  const [emotion, setEmotion] = useState("happy");
+  console.log(emotion)
+  return (
+    <>
+      <h1>Current state of emotions: {emotion}.</h1>
+      <button onClick={() => setEmotion("frustrated")}>
+        Frustrate!
+      </button>
+      <button onClick={() => setEmotion("exhuberant")}>
+        Exhuberate!!
+      </button>
+    </>
+  );
 }
 
 export default App;
