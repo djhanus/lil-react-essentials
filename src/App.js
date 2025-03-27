f4615e210b91019e34868937d9d101b155379e71
djhanus <davidjhanus@gmail.com>
2025-03-27 11:32:03 -0400

    error handling states

--- src/App.js
+++ src/App.js
@@ -5,26 +5,33 @@ import './App.css';
 
 function App() {  
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   const login = 'djhanus';
 
   useEffect(() => {    
+    if (!login) return;
+    setLoading(true);
     fetch(`https://api.github.com/users/${login}`)
     .then((response) => response.json())
-    .then(setData);
-  }, []);
+    .then(setData)
+    .then(() => setLoading(false))
+    .catch(setError);
+  }, [login]);
 
-  if(data) {
-    return (<div>
+  if (loading) return <h1>Loading...</h1>;
+  if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if (!data) return null;
+
+  return (
+    <div>
       <h1>{data.name}</h1>
       <h1>{data.blog}</h1>
       <p>{data.location}</p>
       <img alt={data.login} src={data.avatar_url} />
-    </div>);
-  }
-
-  return <div>No user available -- !!</div>;
- 
+    </div>
+  ); 
 }
 
 export default App;
