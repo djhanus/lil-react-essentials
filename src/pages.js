fd447529f658f5ee16c01f52de05d5565146c35c
djhanus <davidjhanus@gmail.com>
2025-03-27 13:04:40 -0400

    nested routesg

--- src/pages.js
+++ src/pages.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link, useLocation } from "react-router-dom";
+import {Link, useLocation, Outlet } from "react-router-dom";
 
 export function Home() {
     return (
@@ -18,10 +18,34 @@ export function About() {
     return (
         <div>
             <h1>[About Us]</h1>
+            <Outlet />
         </div>  
     );
 }
 
+export function Services() {
+    return (
+        <div>
+            <h2>Services</h2>
+        </div>  
+    );
+}
+
+export function CompanyHistory() {
+    return (
+        <div>
+            <h2>Our History</h2>
+        </div>  
+    );
+}
+
+export function Location() {
+    return (
+        <div>
+            <h2>Location</h2>
+        </div>  
+    );
+}
   
 export function Events() {
     return (
