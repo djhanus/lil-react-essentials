052c35e1dc73b158cd2865720c9b7e19b9bf4a72
djhanus <davidjhanus@gmail.com>
2025-03-27 12:57:42 -0400

    pages

--- src/pages.js
+++ src/pages.js
@@ -1,9 +1,15 @@
 import React from "react";
+import {Link} from "react-router-dom";
 
 export function Home() {
     return (
         <div>
             <h1>[Company Website]</h1>
+            <nav>
+                <Link to="about">About</Link>
+                <Link to="events">Events</Link>
+                <Link to="contact">Contact</Link>
+            </nav>
         </div>  
     );
 }
@@ -31,5 +37,13 @@ export function Contact() {
         <div>
             <h1>[Contact]</h1>
         </div>  
+    );
+}
+
+export function Whoops404() {
+    return (
+        <div>
+            <h1>Whoops, page/route not found</h1>
+        </div>
     );
 }
